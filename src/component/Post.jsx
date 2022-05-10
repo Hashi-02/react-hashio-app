a228c5fe27a22035aa93e1398cacc12b6a63f952
hashi-02 <hassie0209@gmail.com>
2022-05-10 14:52:24 +0900

    Post.jsxにページ遷移の実装

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -1,12 +1,34 @@
 import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
 const Post = () => {
+  const [user, setUser] = useState('');
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+  }, []);
   return (
-    <div>
-      <h1>新規投稿</h1>
-      <Link to={'/Review'}>
-        <button>レビューに戻る</button>
-      </Link>
-    </div>
+    <>
+      {!loading && (
+        <>
+          {!user ? (
+            <Navigate to={`/`} />
+          ) : (
+            <>
+              <h1>新規投稿</h1>
+              <Link to={'/review'}>
+                <button>レビューに戻る</button>
+              </Link>
+            </>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
