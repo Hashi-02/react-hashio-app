b7e4e1b386ef45b7d04c9916710d2ecd6168e2ef
hashi-02 <hassie0209@gmail.com>
2022-05-10 10:54:57 +0900

    firebase認証

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,13 +1,36 @@
-import { Link } from 'react-router-dom';
-const Home = () => {
-  return (
-    <div>
-      <h1>ホームページ</h1>
+import React, { useState, useEffect } from 'react';
+import { auth, provider } from '../firebase';
+import { Navigate } from 'react-router-dom';
+import { onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 
-      <Link to={'/Review'}>
-        <button>ログイン</button>
-      </Link>
-    </div>
+const Home = () => {
+  const handleGoogleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      alert('メールアドレスまたはパスワードが間違っています');
+    }
+  };
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+  return (
+    <>
+      {user ? (
+        <Navigate to={`/review`} />
+      ) : (
+        <>
+          <div>
+            <h1>ホームページ</h1>
+            <button onClick={handleGoogleSubmit}>Googleログイン</button>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
--- src/component/Review.jsx
+++ src/component/Review.jsx
@@ -1,16 +1,45 @@
 import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from '../firebase';
+import { useNavigate, Navigate } from 'react-router-dom';
 const Review = () => {
+  const [user, setUser] = useState('');
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+  }, []);
+  const navigate = useNavigate();
+  const logout = async () => {
+    await signOut(auth);
+    navigate('/');
+  };
   return (
-    <div>
-      <h1>レビュー</h1>
+    <>
+      {!loading && (
+        <>
+          {!user ? (
+            <Navigate to={`/`} />
+          ) : (
+            <>
+              <h1>レビュー</h1>
+              <h1>プロフィール</h1>
+              <p>{user?.email}</p>
+              <p>{user?.displayName}</p>
+              <img src={user?.photoURL} alt="ユーザープロフィール画像" />
 
-      <Link to={'/'}>
-        <button>ログアウト</button>
-      </Link>
-      <Link to={'/post'}>
-        <button>新規投稿</button>
-      </Link>
-    </div>
+              <button onClick={logout}>ログアウト</button>
+              <Link to={'/post'}>
+                <button>新規投稿</button>
+              </Link>
+            </>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
