47b789b9217a3f150bb9067cc404ed3fc335f04c
hashi-02 <hassie0209@gmail.com>
2022-05-19 23:06:18 +0900

    NotFound作成

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './component/Home';
 import Review from './component/Review';
 import Post from './component/Post';
+import NotFound from './component/NotFound';
+import DetailPage from './component/DetailPage';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path={'/'} element={<Home />} />
           <Route path={'/Review'} element={<Review />} />
           <Route path={'/Post'} element={<Post />} />
+          <Route path={`/review/:id`} element={<DetailPage />} />
+          <Route path={`*`} element={<NotFound />} />
           {/* <Route path={'/Detail'} element={<Detail />} />
         <Route path={'/Edit'} element={<Edit />} /> */}
         </Routes>
---
+++ src/component/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <>
+      <h1>お探しのページは見つかりませんでした。</h1>
+      <Link to={`/`}>ホームに戻る</Link>
+    </>
+  );
+};
+export default NotFound;
