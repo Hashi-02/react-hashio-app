5e59fb1edee5ede8be155e05a2c375b586800578
hashi-02 <hassie0209@gmail.com>
2022-05-21 19:35:08 +0900

    reviewページのレスポンシブ 化

--- src/component/Review.jsx
+++ src/component/Review.jsx
@@ -66,7 +66,7 @@ const Review = () => {
                 </Link>
               </div>
 
-              <div className="grid grid-cols-4 gap-4 text-center mx-3">
+              <div className="grid gap-4 text-center mx-3 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
                 {labos.map((d, id) => (
                   <Link to={`/review/${d.Laboratory}`} key={id}>
                     <p className="bg-transparent hover:bg-yellow-500 text-yellow-700 font-semibold hover:text-white py-2 w-full border border-yellow-500 hover:border-transparent rounded">
